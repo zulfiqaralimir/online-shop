bcd71fba23e4f1e5712922da8e8f719a7ad8c04d
Zulfiqar Ali Mir <mir.global.academy@gmail.com>
2025-01-06 12:49:58 +0500

    Fixed Metadata import issue and cleaned up code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import NavBar from "../components/NavBar";
 
-import type { Metadata } from "next";
+// import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
